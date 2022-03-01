bbcb5c1faa9945758bb7b4b6219f36c6b64e6870
Fajrizky <muhammadfajrizky@gmail.com>
2022-03-01 09:06:11 +0800

    Task Ownership and Restrictions

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -3,12 +3,14 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { User } from 'src/auth/user.entity';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
-  async getTasks(filtersDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filtersDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filtersDto;
     const query = this.createQueryBuilder('task');
+    query.where({ user });
 
     if (status) {
       query.andWhere('task.status = :status', { status }); //&&
@@ -16,7 +18,7 @@ export class TasksRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` }, //Percent sign is used to search find by characters
       );
     }
@@ -24,13 +26,14 @@ export class TasksRepository extends Repository<Task> {
     const tasks = await query.getMany();
     return tasks;
   }
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
 
     const task = this.create({
       title,
       description,
       status: TaskStatus.OPEN,
+      user,
     });
 
     await this.save(task);
