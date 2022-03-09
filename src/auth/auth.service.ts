f934bd77e5b371c2297916fb83148a2fc544deb1
Fajrizky <muhammadfajrizky@gmail.com>
2022-03-10 02:09:06 +0800

    fix jwt issue

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,10 +26,24 @@ export class AuthService {
 
     if (user && (await bcrypt.compare(password, user.password))) {
       const payload: JwtPayload = { username };
-      const accessToken = await this.jwtService.sign(payload);
+      const accessToken: string = await this.jwtService.sign(payload);
       return { accessToken };
     } else {
       throw new UnauthorizedException('Invalid credentials');
     }
   }
+  // async signIn(
+  //   authCredentialsDto: AuthCredentialsDto,
+  // ): Promise<{ accessToken: string }> {
+  //   const { username, password } = authCredentialsDto;
+  //   const user = await this.usersRepository.findOne({ username });
+
+  //   if (user && (await bcrypt.compare(password, user.password))) {
+  //     const payload: JwtPayload = { username };
+  //     const accessToken: string = await this.jwtService.sign(payload);
+  //     return { accessToken };
+  //   } else {
+  //     throw new UnauthorizedException('Please check your login credentials');
+  //   }
+  // }
 }
